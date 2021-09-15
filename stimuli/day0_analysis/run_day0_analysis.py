831a34bab217a5487518dd83316d6e6b4629423e
everythingevolves <millman.dan@gmail.com>
2021-09-14 20:19:56 -0700

    fixed bessel mode flag

--- stimuli/day0_analysis/run_day0_analysis.py
+++ stimuli/day0_analysis/run_day0_analysis.py
@@ -10,12 +10,14 @@ import day0_sparse_noise_analysis
 
 mouse_ID = sys.argv[1]
 session_ID = sys.argv[2]
-session_type = sys.argv[3]
 
-exptpath = r'S:\\scientifica_ophys\\data\\' + str(session_ID) + '\\'
+session_type = 'Multiplane'
+if len(sys.argv)>3:
+    session_type = sys.argv[3]
 
 is_bessel = False
 if session_type.lower().find('bessel'):
     is_bessel = True
 
+exptpath = r'S:\\scientifica_ophys\\data\\' + str(session_ID) + '\\'
 day0_sparse_noise_analysis.run_analysis(mouse_ID=mouse_ID,exptpath=exptpath,is_bessel=is_bessel)
